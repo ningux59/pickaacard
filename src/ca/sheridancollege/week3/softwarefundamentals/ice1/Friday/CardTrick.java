416b4cf334428ad4c30c03f98fba9a47ac691fd3
ningux59 <gunin@sheridancollege.ca>
2021-02-05 21:44:16 -0500

    This is the card trick to find card value.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -11,8 +11,14 @@ public class CardTrick {
        
       Card cardA = new Card();
       cardA.setSuit("Clubs");
-       cardA.setValue("5");
-        Systerm.out.println("The new card suit is :"+cardA.getSuit()+"\n The new card value is :"+cardA.getValue());
+      cardA.setValue(5);
+      System.out.println("The new card suit is :"+cardA.getSuit()+"\n The new card value is :"+cardA.getValue());
+     
+      Card cardB = new Card();
+      cardB.setSuit("Dimonds");
+      cardB.setValue(8);
+      System.out.println("The new card suit is :"+cardB.getSuit()+"\n The new card value is :"+cardB.getValue());
     }
+    
     
 }
